3fdd8376b4f2b324661c64e3c214fc0691c9bd0f
Rahul Choudhary <115464962+rahulchoudhary-blitz@users.noreply.github.com>
2022-11-02 13:17:22 +0530

    remove the space

--- src/component/AddCatalogue.js
+++ src/component/AddCatalogue.js
@@ -40,7 +40,7 @@ const AddCatalogue = () => {
   return (
     <Content>
       <Row
-        md={{
+        md ={{
           span: 24,
         }}
       >
@@ -49,26 +49,26 @@ const AddCatalogue = () => {
         </Col>
         <Col span={24}>
           <Form
-            form={form}
-            name="catalogue_form"
-            onFinish={onFinish}
-            autoComplete="off"
+            form = {form}
+            name = "catalogue_form"
+            onFinish = {onFinish}
+            autoComplete = "off"
             {...formLayout}
           >
-            <Row gutter={[12, 12]}>
-              <Col span={24}>
+            <Row gutter = {[12, 12]}>
+              <Col span = {24}>
                 <UploadMedia />
               </Col>
             </Row>
 
             <Col span={24}>
               <Card>
-                <Row gutter={[12, 12]}>
-                  <Col xs={24} lg={12}>
+                <Row gutter = {[12, 12]}>
+                  <Col xs = {24} lg = {12}>
                     <Form.Item
-                      label="Name"
-                      name="name"
-                      rules={[
+                      label = "Name"
+                      name = "name"
+                      rules ={[
                         {
                           required: true,
                           message: "Please enter product name!",
@@ -78,10 +78,10 @@ const AddCatalogue = () => {
                       <Input type="text" placeholder="Name" />
                     </Form.Item>
                   </Col>
-                  <Col xs={24} lg={12}>
+                  <Col xs = {24} lg = {12}>
                     <Form.Item
-                      label="Pick-up Point"
-                      name="pickup-name"
+                      label = "Pick-up Point"
+                      name = "pickup-name"
                       rules={[
                         {
                           required: true,
@@ -99,12 +99,12 @@ const AddCatalogue = () => {
                       </Select>
                     </Form.Item>
                   </Col>
-                  {/* ----------------------------------------------------- */}
+                  {/* */}
                   <Col xs={24} lg={12}>
-                    <Form.Item label="Product Type" name="product_type">
+                    <Form.Item label = "Product Type" name = "product_type">
                       <Select
                         showSearch
-                        filterOption={(input, option) =>
+                        filterOption = {(input, option) =>
                           option.props.children
                             .toLowerCase()
                             .indexOf(input.toLowerCase()) >= 0 ||
@@ -112,7 +112,7 @@ const AddCatalogue = () => {
                             .toLowerCase()
                             .indexOf(input.toLowerCase()) >= 0
                         }
-                        placeholder="product_type"
+                        placeholder = "product_type"
                       >
                         <Select.Option>Chooes Product Type</Select.Option>
                         <Select.Option>Body Art(beauty)</Select.Option>
@@ -123,7 +123,7 @@ const AddCatalogue = () => {
                       </Select>
                     </Form.Item>
                   </Col>
-                  <Col xs={24} lg={12}>
+                  <Col xs = {24} lg = {12}>
                     <Form.Item
                       label="GST (%)"
                       name="gst"
@@ -165,18 +165,16 @@ const AddCatalogue = () => {
                       </Select>
                     </Form.Item>
                   </Col>
-                  {/* ------------------------------------- */}
-                  <Col xs={24} lg={12}>
-                    <Form.Item label="Colour" name={"colour"}>
-                      <Input type="text" placeholder="Color" />
+                  <Col xs = {24} lg = {12}>
+                    <Form.Item label = "Colour" name = {"colour"}>
+                      <Input type = "text" placeholder = "Color" />
                     </Form.Item>
                   </Col>
-                  {/* ---------------------------- */}
-                  <Col xs={24} lg={12}>
+                  <Col xs = {24} lg = {12}>
                     <Form.Item
-                      label="Product Code"
-                      name={"product_id"}
-                      rules={[
+                      label = "Product Code"
+                      name = {"product_id"}
+                      rules ={[
                         {
                           required: true,
                           message: "Please enter Code",
@@ -230,7 +228,7 @@ const AddCatalogue = () => {
                     </Form.Item>
                   </Col>
                   <Col md={{ span: 24 }}>
-                    {/* SkuForm>>>>>>> */}
+                    {/* SkuForm */}
                     <Form.List name="customer_skus">
                       {(fields, { add, remove }) => (
                         <>
@@ -263,6 +261,7 @@ const AddCatalogue = () => {
                       {(fields, { add, remove }) => (
                         <>
                           {fields.map((field, index) => (
+                            // AttributeForm
                             <AttributeForm
                               index={index}
                               field={field}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import CatalogueListData from './pages/catalogueList/CatalogueListData';
-// import SkuForm from './pages/SkuFrom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
